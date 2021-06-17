607a2792deec51f6cab708e4c755047c15093af2
Hugo Folloni <hugofollogua07@gmail.com>
2021-06-17 14:53:37 -0300

    Add files via upload

--- Weather/script.js
+++ Weather/script.js
@@ -1,52 +1,67 @@
-/// NEXT THINGS TO DO: Create a Input to choose location if you don't want to know on your current 
-/// AND: Change the background color if surpass the 18 PM
+var long 
+var lat
+var description = document.getElementById('description');
+var temp = document.getElementById('temperature');
+var nbhood = document.getElementById('location');
+var timelabel = document.getElementById('hourtime') 
+var body = document.getElementById('body');
 
+window.addEventListener('load', getWeather);
 
-window.addEventListener('load', ()=>{
-    let long;
-    let lat;
-    let temperatureDescription = document.querySelector('.temperature-description');
-    let temperatureDegree = document.querySelector('.temperature-degree')
-    let locationTimezone = document.querySelector('.location-timezone')
-
+function getWeather(){
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
-            console.log(position)
-            
-            const proxy = 'https://cors-anywhere.herokuapp.com/';
-            const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`
-            const location = `${proxy}https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=pt`
+            console.log(position);
+
+            var proxy = 'https://cors-anywhere.herokuapp.com/';
+            var api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`
+            var location = `${proxy}https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${long}&localityLanguage=pt`
+            var today = new Date()
+            var hourTime = String(today.getHours()).padStart(2, '0') 
+            var minuteTime = String(today.getMinutes()).padStart(2, '0')
+            var time = hourTime + ':' + minuteTime;
+            console.log(time)
+            timelabel.textContent = time
 
             
             Promise.all([
                 fetch(api),
-                fetch(location)
+                fetch(location),
             ]).then(function (responses) {
                 return Promise.all(responses.map(function (response) {
                     return response.json();
                 }));
             }).then(function (data) {
                 console.log(data)
-                const {temperature, summary, icon } = data[0].currently;
-                const locality = data[1].locality
-                temperatureDegree.textContent = ((temperature - 32) * 5/9).toFixed(1);
-                temperatureDescription.textContent = summary; 
-                locationTimezone.textContent = locality
+                var {temperature, summary, icon } = data[0].currently;
+                var locality = data[1].locality
+                temp.textContent = ((temperature - 32) * 5/9).toFixed(1);
+                description.textContent = summary; 
+                nbhood.textContent = locality
                 setIcons(icon, document.querySelector('.icon'));
             })
+
+            
+            if(hourTime < 18){
+                body.style.cssText = 'background: #2980b9; background: -webkit-linear-gradient(to bottom, #2c3e50, #2980b9); background: linear-gradient(to bottom, #2c3e50, #2980b9);'
+                
+            }
+            else{
+                body.style.cssText = 'background: #948E99; background: -webkit-linear-gradient(to bottom, #2E1437, #948E99);  background: linear-gradient(to bottom, #2E1437, #948E99);'
+            }
+
         })
-    
     } else{
         h1.textContent = 'Hey, please enable your geolocation to us!'
     }
 
-
     function setIcons(icon, iconID){
-        const skycons = new Skycons({color: 'white'})
-        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        var skycons = new Skycons({color: '#eaeaea'})
+        var currentIcon = icon.replace(/-/g, "_").toUpperCase();
         skycons.play();
         return skycons.set(iconID, Skycons[currentIcon]);
     }
-});
+
+} 
